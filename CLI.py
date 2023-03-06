a62e5a83112162b9a7105bb92d86b0454286995f
V4z <da.xvan201@gmail.com>
2023-03-06 18:14:02 +0700

    Reformat code

--- CLI.py
+++ CLI.py
@@ -1,17 +1,54 @@
-from Utils import encrypt, decrypt
+import Utils
 import time
 
-key = '0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f'
-plaintext = '0x00112233445566778899aabbccddeeff'
+lua_chon = 0
+plaintext = ''
+ciphertext = ''
+key = ''
 
-start_time = time.time()
-state = encrypt(plaintext, key)
-end_time = time.time()
-
-print("encryt: ", state)
-print(f'Time taken: {round(end_time - start_time, 4)} seconds')
-start_time = time.time()
-state = decrypt(state, key)
-end_time = time.time()
-print("decryt: ", state)
-print(f'Time taken: {round(end_time - start_time, 4)} seconds')
+while lua_chon != 1 and lua_chon != 2:
+    lua_chon = int(input("Chọn chức năng (1 - Encrypt, 2 - Decrypt): "))
+if lua_chon == 1:
+    dir = input("Nhập vào tên file cần mã hóa: ")
+    while True:
+        try:
+            with open(dir) as file:
+                plaintext = file.read()
+        except:
+            dir = input("File không tồn tại. Nhập lại: ")
+        else:
+            break
+    key = input("Nhập khóa: ")
+    start_time = time.time()
+    blocks = Utils.preprocess_data_input(plaintext)
+    cipher_blocks = []
+    for block in blocks:
+        tmp = Utils.encrypt(block, key)
+        cipher_blocks.append(tmp)
+    cipher_blocks = [Utils.to_ascii(i) for i in cipher_blocks]  # chuyen hex sang ki tu
+    ciphertext = "".join(cipher_blocks)
+    end_time = time.time()
+    print("Kết quả: ", ciphertext)
+    print(f'Thời gian: {round(end_time - start_time, 4)} s')
+elif lua_chon == 2:
+    dir = input("Nhập vào tên file cần giải mã hóa: ")
+    while True:
+        try:
+            with open(dir) as file:
+                ciphertext = file.read()
+        except:
+            dir = input("File không tồn tại. Nhập lại: ")
+        else:
+            break
+    key = input("Nhập khóa: ")
+    start_time = time.time()
+    blocks = Utils.preprocess_data_input(ciphertext)
+    plain_blocks = []
+    for block in blocks:
+        tmp = Utils.decrypt(block, key)
+        plain_blocks.append(tmp)
+    plain_blocks = [Utils.to_ascii(i) for i in plain_blocks]  # chuyen hex sang ki tu
+    plaintext = "".join(plain_blocks)
+    end_time = time.time()
+    print("Kết quả: ", plaintext)
+    print(f'Thời gian: {round(end_time - start_time, 4)} s')
